f6b182ef0d57d1fa90707238d8db2994bc951131
corinja <c.ashwell@comicelief.com>
2020-01-03 18:41:51 +0000

    Added debug config

--- src/Action/Hello.action.js
+++ src/Action/Hello.action.js
@@ -5,15 +5,18 @@ import {
   RequestService,
 } from '@comicrelief/lambda-wrapper';
 
-import CONFIGURATION from '../Config/Configuration';
+import CONFIGURATION, { DEFINITIONS } from '../Config/Configuration';
 
 export default LambdaWrapper(CONFIGURATION, (di: DependencyInjection, request: RequestService, done) => {
   // Get a name from the query parameters.
   const name = request.get('name');
+  const logger = di.get(DEFINITIONS.LOGGER);
 
   const response = new ResponseModel({
     response: name !== null ? `Hello ${name}` : 'Hello',
   }, 200, 'ok');
+
+  logger.info('Log says Hello');
 
   done(null, response.generate());
 });
